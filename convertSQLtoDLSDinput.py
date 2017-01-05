d8d1a059ed23daa3017e111656bbd603ebc24f4e
s58380 <s58380@beuth-hochschule.de>
2017-01-05 08:59:07 +0100

    added convert sql to dlsd input

---
+++ convertSQLtoDLSDinput.py
@@ -0,0 +1,26 @@
+from dlsd.dataset import dataset_sqlToNumpy as stn
+import numpy as np
+import pandas as pd
+import argparse
+
+def getArguments():
+	parser = argparse.ArgumentParser(description="Convert sql output to a wide table")
+	parser.add_argument('-i','--inputFile',help='Path to sql output file',required=True)
+	parser.add_argument('-o','--outputFile',help='Path to write output to',required=True)
+	parser.add_argument('-s','--specifiedSensors',help='python list of sensors to be extracted',required=False)
+
+	args = parser.parse_args()
+	return args
+
+def main():
+	args = getArguments()
+
+
+
+	wide_data = stn.pivotAndSmooth(args.inputFile,
+		None if args.specifiedSensors is None else pd.DataFrame(args.specifiedSensors.split(","),dtype=int))[0]
+	wide_data.to_csv(args.outputFile,index=False,header=True)
+	
+
+if __name__=="__main__":
+	main()
