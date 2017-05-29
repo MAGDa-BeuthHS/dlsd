bb475a798737e44427a644921d55af58d79de989
s58380 <s58380@beuth-hochschule.de>
2017-05-29 18:13:49 +0200

    adding missing files

---
+++ 17_05_4_dlsd_2_trials.py
@@ -0,0 +1,45 @@
+from dlsd_2.input_target_maker.ITM_Normalized_Moving_Average_Remove_Inefficient_Sensors import ITM_Normalized_Moving_Average_Remove_Inefficient_Sensors
+from dlsd_2.model.types.neural_networks.nn_one_hidden_layer.NN_One_Hidden_Layer import NN_One_Hidden_Layer
+from dlsd_2.model.types.average_week.Average_Week import Average_Week
+import logging
+
+logging.basicConfig(level=logging.DEBUG)#filename='17_05_04_dlsd_2_trials.log',)
+
+def main():
+	file_path_train = '/Users/ahartens/Desktop/Work/pzs_oneYear_belegung.csv'
+	file_path_test = '/Users/ahartens/Desktop/Work/pzs_oneYear_belegung.csv'
+
+	# define a model
+	model = NN_One_Hidden_Layer()
+	model.set_number_hidden_nodes(50)
+	model.set_learning_rate(.4)
+	model.set_batch_size(10)
+	model.set_max_steps(50000)
+	model.set_path_saved_session('/Users/ahartens/Desktop/Work/dlsd_2_trials/saved_sess')
+	model.set_path_tf_output('/Users/ahartens/Desktop/Work/dlsd_2_trials')	
+	
+	# 2 define model input/target : this is done using a maker.
+	train_input_and_target_maker = ITM_Normalized_Moving_Average_Remove_Inefficient_Sensors()
+	train_input_and_target_maker.set_source_file_path(file_path_train)
+	train_input_and_target_maker.set_moving_average_window(15)
+	#train_input_and_target_maker.set_time_format('%Y-%m-%d %H:%M:%S')#.%f')
+	train_input_and_target_maker.set_input_sensor_idxs_and_timeoffsets_lists(None,[0]) # use all sensors, no time offset for input
+	train_input_and_target_maker.set_target_sensor_idxs_and_timeoffsets_lists([0],[30,45,60]) # use sensor
+	# 3 train the model
+	model.prepare_data_and_train_with_input_target_maker(train_input_and_target_maker)
+
+	# 4 test the model
+	test_input_and_target_maker = ITM_Normalized_Moving_Average_Remove_Inefficient_Sensors()
+	test_input_and_target_maker.set_source_file_path(file_path_test)
+	#test_input_and_target_maker.set_time_format('%Y-%m-%d %H:%M:%S')	
+	model.prepare_data_and_test_with_input_target_maker(test_input_and_target_maker)
+
+	# 5 write results
+	accuracy_dict = model.calc_prediction_accuracy()
+	model.write_target_and_predictions_to_file('/Users/ahartens/Desktop/preidctions_and_target.csv')
+
+if __name__=="__main__":
+	main()
+
+
+
